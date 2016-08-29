3db7bf322b8d5c995e0460eba9df0eebd729438f
Keresztes Peter-Zoltan <zozo@z0z0.tk>
2016-08-30 02:44:45 +0300

    adding config template and web files

---
+++ webroot/status.php
@@ -0,0 +1,29 @@
+<?php
+$DB_HOST = "localhost";
+$DB_USER = "root";
+$DB_NAME = "mysql";
+
+// Create connection
+$conn = mysql_connect($DB_HOST, $DB_USER);
+$conn1 = mysql_connect($DB_HOST, $DB_USER,,$DB_NAME);
+
+//Check connection
+if (!$conn) {
+	die("Connection failed:" . mysql_connect_error());
+}
+if (!$conn1) {
+	die("Connection failed:" . mysql_connect_error());
+}
+
+$res = mysql_query("SHOW DATABASES");
+$res1 = mysql_query("SELECT DISTINCT User from user");
+
+while ($row = mysql_fetch_assoc($res)) {
+	echo $row['Database'] . "\n";
+}
+
+while ($row1 = mysql_fetch_assoc($res1)) {
+	echo $row1['User'] . "\n";
+}
+
+?>
