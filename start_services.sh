f647ca2646747983f49c7c29a4b441beef3d0fb4
Keresztes Peter-Zoltan <zozo@z0z0.tk>
2016-08-30 20:18:16 +0300

    changing endpoint script

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/etc/init.d/mysql start
-/etc/init.d/php5-fpm start
-/etc/init.d/nginx start
+/usr/sbin/php5-fpm --fpm-config /etc/php5/fpm/php-fpm.conf --daemonize
+/usr/bin/mysqld_safe &
+/usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
